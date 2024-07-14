b59b6d7376767e8b89aba2fc02df37bb2b2f664e
PrathameshPawar-SDET <pawarprathamesh62@gmail.com>
2024-07-14 21:09:41 +0530

    First commit

---
+++ app/src/test/java/fedex_totalsales/DriverSingleton.java
@@ -0,0 +1,27 @@
+package fedex_totalsales;
+
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class DriverSingleton {
+
+    private static WebDriver driver = null;
+
+    private DriverSingleton(){}
+
+    public static WebDriver getdriver() throws MalformedURLException{
+
+        if(driver==null){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            System.out.println("createDriver()");
+        }
+        driver.manage().window().maximize();
+
+        return driver;
+    }
+}
